e3d8bbe303dc2a1134c64ef8c5425fd9aad89323
Michelle Ranly <meranly8@gmail.com>
2020-09-15 02:38:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,13 @@
 katz_deli = []
 
 def line(array)
-  index = array
   if array.size == 0
     puts "The line is currently empty."
   else
     array.each_with_index do |person, position|
-      person[position] 
-      puts "The line is currently: #{position + 1}. #{person}"
+      people_in_line << "#{position + 1}. #{person}"
   end
-  
-  
-  end
+    puts "The line is currently: #{people_in_line}"
 end 
 
 def take_a_number(line, name)
