328b26114a153cad85a73229d8c3354b107ac18c
Michelle Ranly <meranly8@gmail.com>
2020-09-15 02:44:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,10 @@ def line(array)
   else
     array.each_with_index do |person, position|
       people_in_line << "#{position + 1}. #{person}"
+      puts "The line is currently: #{people_in_line.join(" ")}"
     end
   end
-    puts "The line is currently: #{people_in_line.join(" ")}"
+    
 end 
 
 def take_a_number(line, name)
