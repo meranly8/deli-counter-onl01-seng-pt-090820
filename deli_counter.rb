46263c8fb809c6c2a8e30979b72778508e7fa0dc
Michelle Ranly <meranly8@gmail.com>
2020-09-15 02:48:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,9 +9,7 @@ def line(array)
   puts "The line is currently: #{people_in_line.join(" ")}"
   else
     puts "The line is currently empty."
-  
-  
-    
+  end
 end 
 
 def take_a_number(line, name)
